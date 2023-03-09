ee714a87517392744a40ee005cbe8e6635557dec
Heyward Liu <sin55123456@gmail.com>
2023-03-09 08:39:05 +0000

    Add param_list to recieve test parameters.

--- Lab03/calculator_test.py
+++ Lab03/calculator_test.py
@@ -6,26 +6,33 @@ import math
 class ApplicationTest(unittest.TestCase):
     calculator = Calculator()
     param_list = [(1, 1), (1, 2), (1, 3), (1, 4), (1, 5)]
+    raise_param = (1, "1")
 
     def test_add(self):
         for i, j in self.param_list:
             self.assertEqual(self.calculator.add(i, j), i + j)
-        self.assertRaises(TypeError, self.calculator.add(1, "1"))
+        self.assertRaises(
+            TypeError,
+            self.calculator.add(self.raise_param[0], self.raise_param[1])
+        )
 
     def test_divide(self):
         for i, j in self.param_list:
             self.assertEqual(self.calculator.divide(i, j), i / j)
-        self.assertRaises(ZeroDivisionError, self.calculator.divide(1, 0))
+        self.assertRaises(
+            TypeError,
+            self.calculator.divide(self.raise_param[0], self.raise_param[1])
+        )
 
     def test_sqrt(self):
         for _, j in self.param_list:
             self.assertEqual(self.calculator.sqrt(j), math.sqrt(j))
-        self.assertRaises(TypeError, self.calculator.sqrt("1"))
+        self.assertRaises(TypeError, self.calculator.sqrt(self.raise_param[1]))
 
     def test_exp(self):
         for _, j in self.param_list:
             self.assertEqual(self.calculator.exp(j), math.exp(j))
-        self.assertRaises(TypeError, self.calculator.exp("1"))
+        self.assertRaises(TypeError, self.calculator.exp(self.raise_param[1]))
 
 
 if __name__ == '__main__':
