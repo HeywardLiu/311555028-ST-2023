b2df86ab95466545f1646c705af28fdff5d3d44e
Heyward Liu <sin55123456@gmail.com>
2023-03-10 07:30:00 +0000

    Fix error termination of raising from invalid testcase

--- Lab03/calculator_test.py
+++ Lab03/calculator_test.py
@@ -11,28 +11,32 @@ class ApplicationTest(unittest.TestCase):
     def test_add(self):
         for i, j in self.param_list:
             self.assertEqual(self.calculator.add(i, j), i + j)
-        self.assertRaises(
-            TypeError,
-            self.calculator.add(self.raise_param[0], self.raise_param[1])
-        )
+        with self.assertRaises(TypeError):
+            self.calculator.add(
+                self.raise_param[0],
+                self.raise_param[1]
+            )
 
     def test_divide(self):
         for i, j in self.param_list:
             self.assertEqual(self.calculator.divide(i, j), i / j)
-        self.assertRaises(
-            TypeError,
-            self.calculator.divide(self.raise_param[0], self.raise_param[1])
-        )
+        with self.assertRaises(TypeError):
+            self.calculator.divide(
+                self.raise_param[0],
+                self.raise_param[1]
+            )
 
     def test_sqrt(self):
         for _, j in self.param_list:
             self.assertEqual(self.calculator.sqrt(j), math.sqrt(j))
-        self.assertRaises(TypeError, self.calculator.sqrt(self.raise_param[1]))
+        with self.assertRaises(TypeError):
+            self.calculator.sqrt(self.raise_param[1])
 
     def test_exp(self):
         for _, j in self.param_list:
             self.assertEqual(self.calculator.exp(j), math.exp(j))
-        self.assertRaises(TypeError, self.calculator.exp(self.raise_param[1]))
+        with self.assertRaises(TypeError):
+            self.calculator.exp(self.raise_param[1])
 
 
 if __name__ == '__main__':
