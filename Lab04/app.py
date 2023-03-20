3893319bd945000c21a85f4b299be95ba7733b07
Heyward Liu <sin55123456@gmail.com>
2023-03-20 10:12:50 +0000

    test

--- Lab04/app.py
+++ Lab04/app.py
@@ -23,27 +23,30 @@ nycu_url = "https://www.nycu.edu.tw/"
 driver.get(nycu_url)
 driver.maximize_window()
 
-print(driver.current_url)
+
 # driver.find_element("link text", "新聞").click()
 element = WebDriverWait(driver, 5).until(
         EC.element_to_be_clickable((By.LINK_TEXT, "新聞")))
 element.click()
-print(driver.current_url)
 
-time.sleep(10)
-driver.find_element(By.CLASS_NAME, "su-post").click()
+
+# # driver.find_element(By.CLASS_NAME, "su-post").click()
 # element = WebDriverWait(driver, 5).until(
 #     EC.element_to_be_clickable((By.CLASS_NAME, "su-post")))
 # element.click()
-print(driver.current_url)
+# print(driver.current_url)
+element = WebDriverWait(driver, 60).until(
+    lambda d:d.find_elements(By.XPATH, "//a[contains(@href, 'https://www.nycu.edu.tw/news/')]") )[1]
+element.click()
+
 
 # post_title = driver.find_element(
 #                 By.CLASS_NAME, "single-post-title.entry-title").text
 element = WebDriverWait(driver, 5).until(
     lambda d: d.find_element(By.CLASS_NAME, "single-post-title.entry-title"))
 post_title = element.text
-print(driver.current_url)
 print(post_title)
+
 # paragraph_list = driver.find_elements(By.CLASS_NAME, "entry-content.clr")
 paragraph_list = WebDriverWait(driver, 5).until(
     lambda d: d.find_element(By.CLASS_NAME, "entry-content.clr"))
